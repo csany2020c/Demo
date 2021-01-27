a17e6895b62112188b1def5c21e9b45ef8b063ac
tuskeb <tuskebganz@gmail.com>
2021-01-27 13:00:54 +0100

    1. ora 2

--- 1_hello_turtle.py
+++ 1_hello_turtle.py
@@ -5,10 +5,17 @@ from turtle import Screen
 akvarium = Screen()
 ametiszt = Turtle()
 ametiszt.shape("turtle")
-for i in range(100):
-    ametiszt.forward(20)
-    ametiszt.left(57)
-    ametiszt.forward(40)
-    ametiszt.right(19)
+# for i in range(100):
+#     ametiszt.forward(20)
+#     ametiszt.left(57)
+#     ametiszt.forward(40)
+#     ametiszt.right(19)
+
+ametiszt.color('red')
+ametiszt.circle(100)
+
+for i in range(180):
+    ametiszt.forward(2)
+    ametiszt.left(2)
 
 akvarium.mainloop()
