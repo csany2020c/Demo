e0f1d1841cc3e8a45ad3dea2ce3fed2d37e851c6
tuskeb <tuskebganz@gmail.com>
2021-01-28 09:49:45 +0100

    1. órai programok magyarázata

--- 1_hello_object.py
+++ 1_hello_object.py
@@ -1,17 +1,40 @@
+#A HelloWorld egy objektum osztály, amely egybezárja az egy feladathoz tartozó mezőket (a mezőkben adatokat tárolunk)
+# és a metódusaokat (a metódusok pedig az adatokon végeznek műveletet).
+#Az alábbi osztály annyira egyszerű, hogy nem is tartalmaz tárolt értékeket, mezőket, hanem csak 2 db metódust,
+# műveletet, amit a def kulcsszóval kell kezdeni.
+#Az osztályban megírt programkód a futtatás során nem fut le automatikusan.
 class HelloWorld:
+    #Az __init__ metódus az osztály konstruktora. Ez akkor fut le, amikor egy példányt létrehozunk belőle.
+    #Minden példány létrehozásakor lefut.
     def __init__(self):
+        #A szövegeket idézőjelbe kell tenni. A kijratás a konzolba a print("") függvvény hívásával történik.
         print("Hello world")
+        #A függvények más függvényektől is kaphatnak bemeneti értéket.
+        # A self-el saját magára hivatkozik egy objektum példány.
+        # A self.__hash__() metódus egy számot ad eredményként, amely egyedi azonosítója a példánynak.
+        # A print ezt a számot írja ki.
+        #Nagyon fontos a műveleti sorrend értelmezése. Előszösr a self, majd a __hash__() függvény értékelődik ki.
+        #Az így kapott számot kapja meg a print függvény, amely kiértékelése során a konzolba ír.
         print(self.__hash__())
 
+    #Egy másik metódus. Ez nem hajtódik végre automatikusan, meg kell hívni, ha akarjuk, hogy fusson.
     def programozo_leszek(self):
         print("Programozó leszek, ha nagy leszek!")
         print(self.__hash__())
 
 
+#3 példány létrehozása a fent deklarált objektum osztályból. A példányora a, b és c betűvel hivatkozunk a továbbiakban.
+#A hivatkozást referenciának is szoktuk nevezni.
+#A műveleti sorrend szintén fontos. Az értékadás egyike a "jobbról balra" műveleteknek, azaz először a Helloworld()
+# hajtódik végre, majd utána az = művelet, azaz az értéke, referenciája a változóba kerül.
+#Itt megfigyelhető még a konstruktor lefutása is.
 a = HelloWorld()
 b = HelloWorld()
 c = HelloWorld()
 
+#Mivel a, b és c példányok a programunkban létrejöttek, így meghívhatók a benne lévő metódusok, és ha lennének
+#benne mezők, akkor azoknak az adatai is módosíthatók lennének. Majd később lesznek is:)
+#Itt figyeljük meg a kiírt hash értékeket is.
 a.programozo_leszek()
 a.programozo_leszek()
 
@@ -20,3 +43,7 @@ b.programozo_leszek()
 b.programozo_leszek()
 
 c.programozo_leszek()
+
+#Órai analógiával: az objektum osztály olyan, mint amikor készítünk egy nyomóformát, amivel sokszor tudjuk
+#ugyanazt kinyomtatni. Az objektum példány pedig a nyomat, amit kedvünkre színezhetünk, persze csak a megadott
+#vonalakon belül. Minden nyomatot máshogy színezhetünk.
--- 1_hello_turtle.py
+++ 1_hello_turtle.py
@@ -1,21 +1,42 @@
+# Az importálás mások által (vagy akár a mi régi kódunk is lehet) készített programokra való hivatkozás
+# Legalább ezeket nem nekünk kellett megírni:)
+# A Turtle osztály a teknőc nyomóformája, a Screen pedig a képernyőé.
 from turtle import Turtle
 from turtle import Screen
 
 
+# Létrehozzuk a képernyőt, ahol a teknőc szaladgálhat.
 akvarium = Screen()
+# Létrhozunk egy teknőcöt. Ez a sor ismételhető lenne többször, akkor több teknőcünk lenne, de a neveik nem egyezhetnek.
 ametiszt = Turtle()
+# A shape metódussal meg tudjuk változtatni a megjelenő képet.
 ametiszt.shape("turtle")
-# for i in range(100):
-#     ametiszt.forward(20)
-#     ametiszt.left(57)
-#     ametiszt.forward(40)
-#     ametiszt.right(19)
 
+# Ismételld 100-szor
+for i in range(100):
+    # Menj előre 20 pixelt
+    ametiszt.forward(20)
+    # fordulj balra 57 fokot
+    ametiszt.left(57)
+    # Menj előre 40 pixelt
+    ametiszt.forward(40)
+    # fordulj balra 57 fokot
+    ametiszt.right(19)
+    # Menj az ismétlés elejére, és néz meg, hogy kell-e még ismételni
+
+# válts színt pirosra
 ametiszt.color('red')
+# rajzolj egy kört, aminek a sugara 100
 ametiszt.circle(100)
 
+# válts színt zöldre
+ametiszt.color('green')
+# Ismételd 180-szor (hogy kör legyen a végén, mert ugye 2 fokonként fordul és 180-szor ismétel, az 360 fok összesen)
 for i in range(180):
     ametiszt.forward(2)
     ametiszt.left(2)
 
+# Az ablak vezérlőelemeinek futtatása, hogy be tudjuk zárni majd:)
 akvarium.mainloop()
+
+# UI: legközelebb objektum lesz a teknőc viselkedése is:)
