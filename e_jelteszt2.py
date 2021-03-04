e3e040cb743bc013b9a787de9f580b1d2339381d
SzaboBalintK <szabobalint1205@gmail.com>
2021-03-04 10:07:43 +0100

    kocsi

--- e_jelteszt2.py
+++ e_jelteszt2.py
@@ -4,13 +4,15 @@ from e_jelek2 import *
 
 turtle = Turtle()
 screen = Screen()
-# turtle._delay(0)
-# turtle.speed(0)
+turtle._delay(0)
+turtle.speed(0)
 s = Szekreny(turtle)
 s.polc()
-
 j = Jel(turtle)
 j.mintajel()
+s.polc()
+j.kocsi()
+
 
 # turtle.left(270)
 # turtle.forward(200)
