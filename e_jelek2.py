c8e33e491e72cad8f5c0b84fffedda39ac4f7117
Félix <fekete.felix2005@gmail.com>
2021-03-04 10:11:16 +0100

    Fenyofa

--- e_jelek2.py
+++ e_jelek2.py
@@ -17,8 +17,31 @@ class Jel:
         #     self.hold() #Márkus Bence
         #     return
 
-    def mintajel(self):
-        self.turtle.forward(20)
-        self.turtle.right(30)
-        self.turtle.forward(20)
+    # def mintajel(self):
+        # self.turtle.forward(20)
+        # self.turtle.right(30)
+        # self.turtle.forward(20)
+
+
+    def fenyofa(self):
+        self.turtle.right(120)
+        self.turtle.forward(40)
+        self.turtle.left(90)
+        self.turtle.forward(30)
+        self.turtle.left(90)
+        self.turtle.forward(30)
+        self.turtle.left(70)
+        self.turtle.forward(34)
+        self.turtle.right(210)
+        self.turtle.forward(70)
+        self.turtle.left(180)
+        self.turtle.forward(120)
+        self.turtle.left(130)
+        self.turtle.forward(30)
+        self.turtle.right(240)
+        self.turtle.forward(25)
+        self.turtle.left(70)
+        self.turtle.forward(30)
+        self.turtle.left(120)
+        self.turtle.forward(25)
 
