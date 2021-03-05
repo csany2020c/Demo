305bddf08732d32b6f1295dd86764870525ba097
Fatér László <faterlaszlo@gmail.com>
2021-03-05 14:17:48 +0100

    Ceruza ujra

--- e_jelek1.py
+++ e_jelek1.py
@@ -267,35 +267,34 @@ class Jel:
         self.turtle.forward(80)
 
     def ceruza(self):
-        for i in range(2):
-            self.turtle.fillcolor(0.8, 0.1, 0)
+        self.turtle.fillcolor(0.8, 0.1, 0)
+        for i in range(4):
             self.turtle.begin_fill()
-            self.turtle.forward(30)
-            self.turtle.left(-90)
-            self.turtle.forward(100)
-            self.turtle.left(-90)
-            self.turtle.forward(30)
+            self.turtle.forward(30 / 2)
+            self.turtle.right(90)
+            self.turtle.forward(55)
+            self.turtle.forward(30 / 2)
+            self.turtle.right(90)
+            self.turtle.forward(35)
             self.turtle.end_fill()
-            self.turtle.begin_fill()
-        for a in range(1):
-            self.turtle.penup()
-            self.turtle.end_fill()
-            self.turtle.goto(159, 75)
-            self.turtle.pendown()
-            self.turtle.end_fill()
-            self.turtle.begin_fill()
-            for b in range(2):
-                self.turtle.right(-120)
-                self.turtle.forward(60)
-            self.turtle.penup()
-            self.turtle.goto(130, 125)
-            self.turtle.forward(20)
-            self.turtle.left(120)
-            self.turtle.pendown()
 
-            for g in range(2):
-                self.turtle.forward(20 / 2)
+        for h in range(1):
+            self.turtle.begin_fill()
+            self.turtle.forward(15)
+            self.turtle.left(120)
             self.turtle.end_fill()
+            self.turtle.begin_fill()
+            self.turtle.forward(50)
+            self.turtle.left(120)
+            self.turtle.forward(50)
+            self.turtle.backward(25)
+            self.turtle.left(120)
+            self.turtle.forward(25)
+            self.turtle.end_fill()
+
+
+
+
 
     def letra(self):
         self.turtle.left(90)
