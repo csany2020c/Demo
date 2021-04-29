bac71928b0fac46b13e64689425bdacfc75eb250
tuskeb <tuskebganz@gmail.com>
2021-04-29 10:36:58 +0200

    Teszt, apróbb finomítások

--- j_digit.py
+++ j_digit.py
@@ -1,115 +1,114 @@
 from j_segment import *
 
 class Digit:
-    A: Segment
-    B: Segment
-    C: Segment
-    D: Segment
-    E: Segment
-    F: Segment
-    G: Segment
-    H: Segment
+    _A: Segment
+    _B: Segment
+    _C: Segment
+    _D: Segment
+    _E: Segment
+    _F: Segment
+    _G: Segment
 
     def __init__(self, x: int, y: int, size: int):
         s = size * 0.80
         b = size / 25
-        self.A = Segment(x + b, y + size * 2, 0, s)
-        self.B = Segment(x + size, y + size * 2 - b, 270, s)
-        self.C = Segment(x + size, y + b, 90, s)
-        self.D = Segment(x + b, y, 0, s)
-        self.E = Segment(x, y + b, 90, s)
-        self.F = Segment(x, y + size * 2 - b, 270, s)
-        self.G = Segment(x + b, y + size, 0, s)
+        self._A = Segment(x + b, y + size * 2, 0, s)
+        self._B = Segment(x + size, y + size * 2 - b, 270, s)
+        self._C = Segment(x + size, y + b, 90, s)
+        self._D = Segment(x + b, y, 0, s)
+        self._E = Segment(x, y + b, 90, s)
+        self._F = Segment(x, y + size * 2 - b, 270, s)
+        self._G = Segment(x + b, y + size, 0, s)
 
     def _num0(self):
-        self.A.set(True)
-        self.B.set(True)
-        self.C.set(True)
-        self.D.set(True)
-        self.E.set(True)
-        self.F.set(True)
-        self.G.set(False)
+        self._A.set(True)
+        self._B.set(True)
+        self._C.set(True)
+        self._D.set(True)
+        self._E.set(True)
+        self._F.set(True)
+        self._G.set(False)
 
     def _num1(self):
-        self.A.set(False)
-        self.B.set(True)
-        self.C.set(True)
-        self.D.set(False)
-        self.E.set(False)
-        self.F.set(False)
-        self.G.set(False)
+        self._A.set(False)
+        self._B.set(True)
+        self._C.set(True)
+        self._D.set(False)
+        self._E.set(False)
+        self._F.set(False)
+        self._G.set(False)
 
     def _num2(self):
-        self.A.set(True)
-        self.B.set(True)
-        self.C.set(False)
-        self.D.set(True)
-        self.E.set(True)
-        self.F.set(False)
-        self.G.set(True)
+        self._A.set(True)
+        self._B.set(True)
+        self._C.set(False)
+        self._D.set(True)
+        self._E.set(True)
+        self._F.set(False)
+        self._G.set(True)
 
     def _num3(self):
-        self.A.set(True)
-        self.B.set(True)
-        self.C.set(True)
-        self.D.set(True)
-        self.E.set(False)
-        self.F.set(False)
-        self.G.set(True)
+        self._A.set(True)
+        self._B.set(True)
+        self._C.set(True)
+        self._D.set(True)
+        self._E.set(False)
+        self._F.set(False)
+        self._G.set(True)
 
     def _num4(self):
-        self.A.set(False)
-        self.B.set(True)
-        self.C.set(True)
-        self.D.set(False)
-        self.E.set(False)
-        self.F.set(True)
-        self.G.set(True)
+        self._A.set(False)
+        self._B.set(True)
+        self._C.set(True)
+        self._D.set(False)
+        self._E.set(False)
+        self._F.set(True)
+        self._G.set(True)
 
     def _num5(self):
-        self.A.set(True)
-        self.B.set(False)
-        self.C.set(True)
-        self.D.set(True)
-        self.E.set(False)
-        self.F.set(True)
-        self.G.set(True)
+        self._A.set(True)
+        self._B.set(False)
+        self._C.set(True)
+        self._D.set(True)
+        self._E.set(False)
+        self._F.set(True)
+        self._G.set(True)
 
     def _num6(self):
-        self.A.set(True)
-        self.B.set(False)
-        self.C.set(True)
-        self.D.set(True)
-        self.E.set(True)
-        self.F.set(True)
-        self.G.set(True)
+        self._A.set(True)
+        self._B.set(False)
+        self._C.set(True)
+        self._D.set(True)
+        self._E.set(True)
+        self._F.set(True)
+        self._G.set(True)
 
     def _num7(self):
-        self.A.set(True)
-        self.B.set(True)
-        self.C.set(True)
-        self.D.set(False)
-        self.E.set(False)
-        self.F.set(False)
-        self.G.set(False)
+        self._A.set(True)
+        self._B.set(True)
+        self._C.set(True)
+        self._D.set(False)
+        self._E.set(False)
+        self._F.set(False)
+        self._G.set(False)
 
     def _num8(self):
-        self.A.set(True)
-        self.B.set(True)
-        self.C.set(True)
-        self.D.set(True)
-        self.E.set(True)
-        self.F.set(True)
-        self.G.set(True)
+        self._A.set(True)
+        self._B.set(True)
+        self._C.set(True)
+        self._D.set(True)
+        self._E.set(True)
+        self._F.set(True)
+        self._G.set(True)
 
     def _num9(self):
-        self.A.set(True)
-        self.B.set(True)
-        self.C.set(True)
-        self.D.set(True)
-        self.E.set(False)
-        self.F.set(True)
-        self.G.set(True)
+        self._A.set(True)
+        self._B.set(True)
+        self._C.set(True)
+        self._D.set(True)
+        self._E.set(False)
+        self._F.set(True)
+        self._G.set(True)
 
     def setNum(self, num: int):
         if num == 0:
@@ -145,8 +144,7 @@ class Digit:
 
 
 # d = Digit(x = 0, y = 0, size = 50)
-# for i in range(10):
-#     d.setNum(i)
-# Digit(x = 100, y = 0, size = 60)
-# Digit(x = 100, y = 190, size = 60)
+# d.setNum(2)
+# d2 = Digit(x = 100, y = 0, size = 100)
+# d2.setNum(7)
 # Screen().mainloop()
--- j_segment.py
+++ j_segment.py
@@ -1,49 +1,54 @@
 from turtle import *
 
 class Segment:
-    t: Turtle
-    size: int
-    isOn: bool = True
+    _t: Turtle
+    _size: int
+    _isOn: bool = True
 
     def __init__(self, x:int, y:int, rotate: int, size:int):
-        self.t = Turtle()
-        self.t.speed(0)
-        self.t._delay(0)
-        self.size = size
-        self.t.hideturtle()
-        self.t.penup()
-        self.t.goto(x,y)
-        self.t.setheading(rotate)
-        self.t.pendown()
+        self._t = Turtle()
+        self._t.speed(0)
+        self._t._delay(0)
+        self._size = size
+        self._t.hideturtle()
+        self._t.penup()
+        self._t.goto(x, y)
+        self._t.setheading(rotate)
+        self._t.pendown()
         self.set(False)
 
     def _paint(self, r: float, g: float, b: float):
-        self.t.clear()
-        self.t.fillcolor(r, g, b)
-        self.t.begin_fill()
-        self.t.left(45)
-        self.t.forward(self.size / 5)
-        self.t.right(45)
-        self.t.forward(self.size - (self.size / 5 / 1.4142135623730950488016887242097))
-        self.t.right(45)
-        self.t.forward(self.size / 5)
-        self.t.right(90)
-        self.t.forward(self.size / 5)
-        self.t.right(45)
-        self.t.forward(self.size - (self.size / 5 / 1.4142135623730950488016887242097))
-        self.t.right(45)
-        self.t.forward(self.size / 5)
-        self.t.right(135)
-        self.t.end_fill()
+        self._t.clear()
+        self._t.fillcolor(r, g, b)
+        self._t.begin_fill()
+        self._t.left(45)
+        self._t.forward(self._size / 5)
+        self._t.right(45)
+        self._t.forward(self._size - (self._size / 5 / 1.4142135623730950488016887242097))
+        self._t.right(45)
+        self._t.forward(self._size / 5)
+        self._t.right(90)
+        self._t.forward(self._size / 5)
+        self._t.right(45)
+        self._t.forward(self._size - (self._size / 5 / 1.4142135623730950488016887242097))
+        self._t.right(45)
+        self._t.forward(self._size / 5)
+        self._t.right(135)
+        self._t.end_fill()
 
     def set(self, on: bool):
-        if self.isOn == on:
+        if self._isOn == on:
             return
-        self.isOn = on
+        self._isOn = on
         if on:
             self._paint(0, 0.8, 0)
         else:
             self._paint(0, 0.2, 0)
 
-# Segment(0,0,0,100)
+#
+# s = Segment(100,0,20,100)
+# s.set(False)
+# s2 = Segment(130,30,-50,100)
+# s2.set(True)
+#
 # Screen().mainloop()
